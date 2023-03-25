fb87b35fe4760d56c2f983d2d4498a1f1654f130
Aine <aine@etke.cc>
2023-03-25 16:43:50 +0200

    add verbose mode

--- main.go
+++ main.go
@@ -11,12 +11,14 @@ var (
 	rolesPath              string
 	requirementsPath       string
 	updateRequirementsFile bool
+	verbose                bool
 )
 
 func main() {
 	flag.StringVar(&requirementsPath, "r", "requirements.yml", "ansible-galaxy requirements file")
 	flag.StringVar(&rolesPath, "p", "roles/galaxy/", "path to install roles")
 	flag.BoolVar(&updateRequirementsFile, "u", false, "update requirements file if newer versions are available")
+	flag.BoolVar(&verbose, "v", false, "verbose output")
 	flag.Parse()
 
 	log.Println("parsing", requirementsPath)
@@ -37,6 +39,14 @@ func execute(command string, dir string) (string, error) {
 	cmd := exec.Command(slice[0], slice[1:]...)
 	cmd.Dir = dir
 	out, err := cmd.CombinedOutput()
+	if verbose {
+		log.Println("DEBUG: execute")
+		log.Println("       command:", command)
+		log.Println("       chdir:", dir)
+		if out != nil {
+			log.Println("       output:", strings.TrimSuffix(string(out), "\n"))
+		}
+	}
 	if out == nil {
 		return "", err
 	}
